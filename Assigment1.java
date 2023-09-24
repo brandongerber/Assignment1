82d3e5a3e22d1833563b5243c58b8a310ab0ab7e
brandongerber <90160923+brandongerber@users.noreply.github.com>
2023-09-24 15:03:52 -0400

    Add files via upload

---
+++ Assigment1.java
@@ -0,0 +1,21 @@
+/**
+ * Program: Assigment1
+ * Date: 09/23/23
+ * Description: I created this program to be show two equations related to PI.
+ */
+public class Assigment1 {
+	
+	public static void main(String[] args) {
+		System.out.println("I wrote a program that displays the result of two equations. ");
+			// I used double to be able to attain decimal approximation
+		System.out.println ("Equation 1 = 4.0 * (1.0 - (1.0/3) + (1.0/5) - (1.0/7) + (1.0/9) - (1.0/11)");
+				System.out.println ("Equation 2 = 4.0 * (1.0 - (1.0/3) + (1.0/5) - (1.0/7) + (1.0/9) - (1.0/11) + (1.0/13)");
+	        double equation1 = 4.0 * (1.0 - (1.0/3) + (1.0/5) - (1.0/7) + (1.0/9) - (1.0/11));
+	        double equation2 = 4.0 * (1.0 - (1.0/3) + (1.0/5) - (1.0/7) + (1.0/9) - (1.0/11 + 1.0/13));
+	        // I then proceeded to print both equations out
+	        System.out.println("Equation 1 approximation: " + equation1);
+	        System.out.println("Equation 2 Approximation : " + equation2);
+	}
+	    }
+	
+
